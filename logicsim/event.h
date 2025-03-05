19fa78cf2ccc4d20825098bd91230d8f66d1d794
Kamsi Nwabueze <knwabuez@usc.edu>
2025-03-05 22:49:35 +0000

    final commit for hw3

--- logicsim/event.h
+++ logicsim/event.h
@@ -1,7 +1,7 @@
 #ifndef EVENT_H
 #define EVENT_H
 #include "wire.h"
-#include "../heap.h"
+
 struct Event
 {
     uint64_t time;
@@ -12,7 +12,7 @@ struct Event
 typedef struct EventLess {
     // Comparison function for creating a min-heap based on event time
     bool operator()(Event* a, Event* b) const {
-        return a->time > b->time;  // Larger time means lower priority in min-heap
+        return a->time < b->time;  // Larger time means lower priority in min-heap
     }
 } EventLess;
 #endif
